7847f31d383216b8300153275d1973778b454e51
earjsin <arjun.rjit@gmail.com>
2026-04-03 22:14:56 +0200

    Add new plain website version - simplified design based on original

---
+++ plain/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const counters = document.querySelectorAll(".counter");
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute("data-target");
+            const count = +counter.innerText;
+            const speed = target / 100;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + speed);
+                setTimeout(updateCount, 30);
+            } else {
+                counter.innerText = target;
+            }
+        };
+        updateCount();
+    });
+});
